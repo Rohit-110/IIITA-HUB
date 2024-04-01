920a0a8df56fd13b80fe9967db42fcfcf79e2c79
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2024-04-01 18:32:39 +0530

    Delete src directory

--- src/Components/Background.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import bgImage from '../images/bg.jpg'
-export default function Background() {
-  return (
-    <div>
-        <img className="h-100 w-1screen" src={bgImage} alt=""/>
-    </div>
-  )
-}
