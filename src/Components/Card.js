86ce1eb65c4acdbeb316ca4bf2e50439bd5bf7f9
CodinJack <115005390+CodinJack@users.noreply.github.com>
2024-07-09 14:57:20 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Card = ({ name }) => {
+const Card = ({ data }) => {
   const profilePath = `/profile/${name}`;
   return (
     <Link to={profilePath}>
       <div className="cursor-pointer card bg-white rounded-lg shadow-md mx-2" style={{ width: "150px", flex: "0 0 auto" }}>
         <img src="https://archive.org/download/twitter-default-pfp/e.png" className="card-img-top w-full h-40 object-cover" alt="Profile" />
         <div className="card-body p-2">
-          <p className="card-text text-center text-gray-800 font-semibold">{name}</p>
+          <p className="card-text text-center text-gray-800 font-semibold">{data.name}</p>
         </div>
       </div>
     </Link>
